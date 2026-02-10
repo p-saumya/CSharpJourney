8c6ca3779644c05cf3d46b92c3ee7a547bca0b51
p-saumya <panch.saumya@gmail.com>
2026-02-10 19:02:54 +0530

    Create sum.cs

---
+++ Main/sum.cs
@@ -0,0 +1,9 @@
+class Sum{
+    public static void Main(String[] ___)
+    {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.Console.WriteLine("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
